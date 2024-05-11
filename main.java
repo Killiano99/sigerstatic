c04901e3b0fadbbf526e8b90e68ce5266ec5fcb4
nfried98 <103784009+nfried98@users.noreply.github.com>
2024-05-11 15:35:16 -0400

    Update main.java

---
+++ main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "Nicholas Friedlander";
+        int apScore = 0;
+        if (name.equals("Nicholas Friedlander")) {
+            apScore = 1;
+        }
+        System.out.println(name + " got a " + apScore + " on the APCSA test!");
+    }
+}
