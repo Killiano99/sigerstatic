c653a8ec2bf9f758c5c02caf3f39bf83fc816f92
MorrisonJ <aidanmorrisonj@gmail.com>
2024-05-18 19:58:25 -0400

    Slots
    Slots working

--- Main.java
+++ Main.java
@@ -1,26 +1,6 @@
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
-        Stock gamestop = new Stock("Gamestop", 100, 0.5);
-        advanceManyDays(gamestop, 1000);
-        
-    }
-
-    public static void advanceManyDays(Stock stock, int days) {
-        for (int i=0; i<days; i++)
-        {
-            stock.advanceDay();
-        }
-        ArrayList<Double> prices = stock.getPreviousPrices();
-        System.out.println(Stock.toPriceDecimal(prices.get(0)));
-        for(int i = 1; i < prices.size(); i++) {
-            if(prices.get(i) > prices.get(i - 1)) {
-                System.out.println("\u001B[32m" + Stock.toPriceDecimal(prices.get(i)) + "\u001B[0m");
-            }
-            else {
-                System.out.println("\u001B[31m" + Stock.toPriceDecimal(prices.get(i)) + "\u001B[0m");
-            }
-        }
+        Casino slots = new Casino(99943); 
+        slots.play();
     }
 }
